3ae266c7fc826319de8c5437c25567499b5681ff
Andrei Chabatul <andreichabatul@gmail.com>
2024-01-17 11:21:58 +0300

    refactoring frontend

--- game-romb-server/backend/src/main.ts
+++ game-romb-server/backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://localhost',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
